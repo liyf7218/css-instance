b109e80299b01795dff15da4aa46b36b7a539420
unit-studio <unitstudio@163.com>
2021-03-12 21:00:20 +0800

    Init project with vue/cli@4.   Wanna use router and vuex? try `vue-ui`.   Wanna use sass? try `npm install -D sass-loader@10.1.1 node-sass@5.0.0` ---- it's very important!

---
+++ src/views/BasicFlex.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="flex">
+    <div class="basic-flex">
+      <div class="item">1</div>
+      <div class="item">2</div>
+      <div class="item">3</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicFlex',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.basic-flex {
+  width: 100%;
+  height: 500px;
+  display: flex;
+
+  .item {
+
+  }
+}
+</style>
